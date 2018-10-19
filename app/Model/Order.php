1f6faf4f60effb876d2229d47d211e6fd081d1eb
Lukas Fam <takeruadelbert@gmail.com>
2018-10-19 12:51:34 +0700

    fix bug not increment ID order number

--- app/Model/Order.php
+++ app/Model/Order.php
@@ -34,7 +34,12 @@ class Order extends AppModel {
         $mRoman = romanic_number($m);
         $Y = date('Y');
         $testCode = "ORD/$mRoman/$Y/[0-9]{5}";
-        $lastRecord = $this->find('first', array('conditions' => array('and' => array("Order.no_order regexp" => $testCode)), 'order' => "CAST(Order.no_order as SIGNED) DESC", 'recursive' => -1));
+        $lastRecord = $this->find('first', array(
+            'conditions' => array(
+                "Order.no_order regexp" => $testCode
+            ), 
+            'order' => "CAST(Order.no_order as SIGNED) DESC"
+        ));
         if (!empty($lastRecord)) {
             $current = explode("/", $lastRecord['Order']['no_order']);
             $inc_id += $current[count($current) - 1];
